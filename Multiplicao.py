158345c0138ef028849b493e4874a32a831f0f99
Joshua <joshuadiasbf@gmail.com>
2024-12-05 22:41:03 -0300

    Soma Pares

--- Multiplicao.py
+++ Multiplicao.py
@@ -1,5 +1,5 @@
 mult = 1
 
-for i in range(1, 5):
+for i in range(1, 6):
     mult *= i
 print(mult)
---
+++ SomaPares.py
@@ -0,0 +1,6 @@
+soma_pares = 0
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        soma_pares += i
+    print("Contador:", i,"Soma Dos Pares:", soma_pares)
