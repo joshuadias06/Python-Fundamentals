f91f3207a74b58e5372e782c87aa645039abdc32
Joshua <joshuadiasbf@gmail.com>
2024-12-05 22:45:31 -0300

    Att Soma Pares

--- SomaPares.py
+++ SomaPares.py
@@ -4,3 +4,10 @@ for i in range(1, 11):
     if i % 2 == 0:
         soma_pares += i
     print("Contador:", i,"Soma Dos Pares:", soma_pares)
+
+
+for j in range(0, 100, 2):
+    if j % 2 == 0:
+        soma_pares += j
+
+print(f"Existem {j / 2} numeros pares de 1 a 100 e a soma deles é{soma_pares}")
