2e47c45853d537b25352d23a7aa059c2f45a3a1d
Joshua <joshuadiasbf@gmail.com>
2024-12-10 20:51:13 -0300

    Retangulo

---
+++ Retangulo.py
@@ -0,0 +1,11 @@
+#For criando um retângulo!
+
+largura = 5
+altura = 3
+
+for i in range(altura):
+    for j in range(largura):
+
+        print("*", end = " ")
+
+    print() 
---
+++ Triangulo.py
@@ -0,0 +1,5 @@
+altura = 5
+
+for i in range(altura):
+    
+
