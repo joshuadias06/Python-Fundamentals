ec0a1d18a06275e398d9c30e0b9919da89c0cea8
Joshua <joshuadiasbf@gmail.com>
2024-12-10 21:13:53 -0300

    origin main

---
+++ FuncaoStringNums.py
@@ -0,0 +1,9 @@
+def saudacao(nome):
+    return f"Olá {nome}, seja bem vindo ao nosso programa de estudos intensivos!"
+
+def soma(a, b):
+    return a + b
+
+resultado = soma(3, 4)
+print(saudacao("Joshua"))
+print("Resultado: ", resultado)
