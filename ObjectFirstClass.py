be723622b72600b65d2ee78e0044756616a1d91e
Joshua <joshuadiasbf@gmail.com>
2024-12-12 19:00:01 -0300

    Atribuindo Funcoes a vars

---
+++ ObjectFirstClass.py
@@ -0,0 +1,9 @@
+print("\n-----------------------\n")
+def saudacao():
+    return "Olá Mundo!"
+
+cumprimento = saudacao
+print(cumprimento())
+print(saudacao())
+print("\n-----------------------\n")
+
