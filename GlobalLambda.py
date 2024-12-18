d2a17f62434c769a5f84ab737d2faee6fcbd31fb
Joshua <joshuadiasbf@gmail.com>
2024-12-18 18:04:37 -0300

    Global e Lambda

---
+++ GlobalLambda.py
@@ -0,0 +1,12 @@
+cont = 0
+def aumentar_cont():
+    global cont
+    cont += 1
+
+aumentar_cont()
+aumentar_cont()
+aumentar_cont()
+print(cont)
+
+#aumentar_cont = lambda: global cont;  cont +=1
+#ERROR
