dc6d49db652513e9c63d33c69ea78cf9d1316698
Joshua Dias <joshuadiasbf@gmail.com>
2024-12-19 20:51:52 -0300

    Ex Slicing

---
+++ List/SlicingEx.py
@@ -0,0 +1,10 @@
+cores = "vermelho", "verde", "azul", "amarelo", "laranja", "roxo", "marrom", "cinza"
+
+select_colors = cores[1:3]
+print(select_colors)
+select_after_yellow = cores[3:]
+print(select_after_yellow)
+select_impar_index = cores[1:8:2]
+print(select_impar_index)
+reverse_cores = cores[::-1]
+print(reverse_cores)
