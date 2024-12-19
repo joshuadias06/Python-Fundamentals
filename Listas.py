faff7e5e469ad1411c4d3a55d6b85fd7a340b1c6
Joshua Dias <joshuadiasbf@gmail.com>
2024-12-18 21:06:32 -0300

    Lista

---
+++ Listas.py
@@ -0,0 +1,17 @@
+#Listas
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+nomes = [ "Joshua", "Queren", "Davi", "Sophia", "Rebeca"]
+
+#Listas
+numeros[0] = 100
+numeros[1] = 50
+numeros[2] = 25
+print(numeros)
+print("===========================================")
+
+#Tuplas
+tupla = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+#tupla[0] = 100 -> ERROR, tuplas sao imutaveis
+print(tupla)
+print(nomes[-2])
