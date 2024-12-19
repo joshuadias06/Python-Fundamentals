7fb1a1ab7e13de0f6e88b5677a5230e859c460e2
Joshua Dias <joshuadiasbf@gmail.com>
2024-12-19 20:45:46 -0300

    List Slicing

---
+++ List/ListSlicing.py
@@ -0,0 +1,18 @@
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+nomes = [ "Joshua", "Queren", "Davi", "Sophia", "Rebeca"]
+sobrenomes = ["Dias", "Elizama", "Dias"]
+
+#Slicing = Fatiamento
+
+sub_nums = numeros[1:4]
+print(sub_nums)
+prim_nums = numeros[:2]
+print(prim_nums)
+dps_dois = numeros[2:]
+print(dps_dois)
+passos_nums = numeros[::2]
+print(passos_nums)
+
+sub_alternado = numeros[2:8:2]
+print(sub_alternado)
+
