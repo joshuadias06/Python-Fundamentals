86c0db4375ea60264c765584fe0961109fe0dac2
Joshua Dias <joshuadiasbf@gmail.com>
2024-12-19 22:49:19 -0300

    Iteraction Lists

---
+++ List/IteractionList.py
@@ -0,0 +1,9 @@
+#Iteracao em listas
+
+frutas = ["maca", "banana", "cereja", "damasco", "figo"]
+
+for fruta in frutas:
+    print(fruta)
+
+for indice, fruta in enumerate(frutas):
+    print(f"Frutas no indice: {indice} e {fruta}")
